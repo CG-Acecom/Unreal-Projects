2d6a5b0d798d68a2cb537e77434264c899655fab
Rogger794 <rogger_jrj794@hotmail.com>
2017-04-19 01:46:33 -0500

    primera

---
+++ PrimeraAplicacion/Source/PrimeraAplicacion/PrimeraAplicacionGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PrimeraAplicacion.h"
+#include "PrimeraAplicacionGameModeBase.h"
+
+
+
+
---
+++ PrimeraAplicacion/Source/PrimeraAplicacion/PrimeraAplicacionGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "PrimeraAplicacionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRIMERAAPLICACION_API APrimeraAplicacionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
