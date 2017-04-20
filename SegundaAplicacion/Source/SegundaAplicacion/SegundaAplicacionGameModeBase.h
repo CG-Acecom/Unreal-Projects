54743b33d6e0b9f3a87e58634d798f0310b43441
Rogger794 <rogger_jrj794@hotmail.com>
2017-04-19 22:33:32 -0500

    segundaAplicacion

---
+++ SegundaAplicacion/Source/SegundaAplicacion/SegundaAplicacionGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "SegundaAplicacionGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SEGUNDAAPLICACION_API ASegundaAplicacionGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
